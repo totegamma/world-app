689b3c90711cb060e93dfafbd5df92935cd020d5
totegamma <github@gammalab.net>
2025-11-26 23:47:47 +0900

    add message creation

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,40 @@
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { Text, StyleSheet, Button, TextInput } from 'react-native';
 import { useClient } from '../context/client';
+import { useState } from 'react';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { commit } from '../lib/client';
 
 export default function Tab() {
 
     const client = useClient();
+    const [draft, setDraft] = useState<string>("");
 
     return (
-        <View style={styles.container}>
-            <Text>CCID: {client.identity?.CCID}</Text>
-            <Text>Registration</Text>
-            <Button title="register" onPress={() => client.register?.()} />
-        </View>
+        <SafeAreaProvider>
+            <SafeAreaView style={styles.container}>
+                <Text>CCID: {client.identity?.CCID}</Text>
+                <Text>Registration</Text>
+                <Button title="register" onPress={() => client.register?.()} />
+                <Text>Message</Text>
+                <TextInput
+                    style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: '100%' }}
+                    onChangeText={text => setDraft(text)}
+                    value={draft}
+                />
+                <Button title="Commit Message" onPress={() => {
+                    if (!client.identity) return;
+                    const document = {
+                        schema: "https://schema.concrnt.world/m/markdown.json",
+                        value: {
+                            "body": draft
+                        },
+                        author: client.identity.CCID,
+                        createdAt: new Date(),
+                    };
+                    commit('cc2.tunnel.anthrotech.dev', client.identity, document)
+                }} />
+            </SafeAreaView>
+        </SafeAreaProvider>
   );
 }
 
--- lib/client.ts
+++ lib/client.ts
@@ -1,6 +1,40 @@
 import { Document, Affiliation } from "./model";
 import { Identity, Sign } from "./crypto";
 
+
+export const commit = async (server: string, identity: Identity, document: Document<any>) => {
+
+    const docString = JSON.stringify(document)
+    const signature = Sign(identity.privateKey, docString)
+
+    const signedDoc = {
+        document: docString,
+        proof: {
+            type: "concrnt-ecrecover-direct",
+            signature: signature
+        }
+    }
+
+    return fetch(`https://${server}/commit`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(signedDoc)
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error(`Server responded with ${response.status}`);
+        }
+        return response.json();
+    }).then(data => {
+        console.log("Affiliation committed successfully:", data);
+    }).catch(error => {
+        console.error("Error committing affiliation:", error);
+    });
+}
+
+
+
 export const affiliate = async (identity: Identity, server: string) => {
 
     const doc: Document<Affiliation> = {
